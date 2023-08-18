4fb49ecdab11d8102d25ca80cf3abc7aa45792fa
Mohammad Eskandari <good_teacher_ey@hotmail.com>
2023-08-18 13:52:26 +0330

    migration for new column

--- database/migrations/2019_12_22_015115_create_short_urls_table.php
+++ database/migrations/2019_12_22_015115_create_short_urls_table.php
@@ -16,7 +16,6 @@ class CreateShortUrlsTable extends Migration
         Schema::create('short_urls', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->text('destination_url');
-            $table->string('prefix')->nullable();
             $table->string('url_key')->unique();
             $table->string('default_short_url');
             $table->boolean('single_use');
